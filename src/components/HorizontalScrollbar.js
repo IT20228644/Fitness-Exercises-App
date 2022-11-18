585ff253a545f0971ff3c9a1f2972c8af78915cc
it20228644 <it20228644@my.sliit.lk>
2022-11-18 12:12:50 +0530

    hor bar & bodypart changed

--- src/components/HorizontalScrollbar.js
+++ src/components/HorizontalScrollbar.js
@@ -36,22 +36,22 @@ const RightArrow = () => {
 };
 
 
-const HorizontalScrollbar = ({data, bodyPart, setBodyPart}) => {
-  return (
-    <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow}>
-        {data.map((item) => (
-            <Box
-              key={item.id || item}
-              itemId={item.id || item}
-              title={item.id || item}
-              m="0 40px"
-            >
-              <BodyPart item={item} bodyPart ={bodyPart} setBodyPart={setBodyPart} />
-            </Box>
-        ))}
-    </ScrollMenu>
-  )
-}
+// const HorizontalScrollbar = ({data, bodyPart, setBodyPart}) => {
+//   return (
+//     <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow}>
+//         {data.map((item) => (
+//             <Box
+//               key={item.id || item}
+//               itemId={item.id || item}
+//               title={item.id || item}
+//               m="0 40px"
+//             >
+//               <BodyPart item={item} bodyPart ={bodyPart} setBodyPart={setBodyPart} />
+//             </Box>
+//         ))}
+//     </ScrollMenu>
+//   )
+// }
 
 export default HorizontalScrollbar
 
@@ -94,19 +94,19 @@ export default HorizontalScrollbar
 //   );
 // };
 
-// const HorizontalScrollbar = ({ data, bodyParts, setBodyPart, bodyPart }) => (
-//   <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow}>
-//     {data.map((item) => (
-//       <Box
-//         key={item.id || item}
-//         itemId={item.id || item}
-//         title={item.id || item}
-//         m="0 40px"
-//       >
-//         {bodyParts ? <BodyPart item={item} setBodyPart={setBodyPart} bodyPart={bodyPart} /> : <ExerciseCard exercise={item} /> }
-//       </Box>
-//     ))}
-//   </ScrollMenu>
-// );
+const HorizontalScrollbar = ({ data, bodyParts, setBodyPart, bodyPart }) => (
+  <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow}>
+    {data.map((item) => (
+      <Box
+        key={item.id || item}
+        itemId={item.id || item}
+        title={item.id || item}
+        m="0 40px"
+      >
+        {bodyParts ? <BodyPart item={item} setBodyPart={setBodyPart} bodyPart={bodyPart} /> : <ExerciseCard exercise={item} /> }
+      </Box>
+    ))}
+  </ScrollMenu>
+);
 
 // export default HorizontalScrollbar;
